1a793a838f69e925a6c9155dcb51e6a46a93384a
RaheemWilson <wilsonzackery16@gmail.com>
2021-10-05 21:56:29 -0500

    Added restart functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,8 @@
-let grid = new Array(9).fill("");
+
 
 window.onload = function () {
+    let grid = new Array(9).fill("");
+    let status = document.getElementById("status")
     let board = document.getElementById("board");
     let boardDivs = [...board.children];
     
@@ -15,7 +17,7 @@ window.onload = function () {
             div.classList.add(nextPlay)
             div.innerHTML = nextPlay;
             grid[parseInt(div.id)] = nextPlay
-            // checkGrid()
+            checkGrid()
             nextPlay = nextPlay === "X" ? "O" : "X";      
         })
 
@@ -25,6 +27,33 @@ window.onload = function () {
 
         div.addEventListener('mouseout', () => {
             div.classList.remove("hover")
+        })
+    })
+    
+    const checkGrid = () => {
+    
+        let gridstr = grid.join("")
+        console.log(gridstr)
+        if(gridstr.includes("XXX")){
+            status.classList.add("you-won")
+            status.innerHTML = "Congratulations! X is the Winner!"
+        } else if(gridstr.includes("OOO")){
+            status.classList.add("you-won")
+            status.innerHTML = "Congratulations! O is the Winner!"
+        }
+    }
+
+    let btn = document.getElementsByClassName("btn")[0]
+    btn.addEventListener('click', () => {
+        status.classList.remove("you-won")
+        status.innerHTML = "Move your mouse over a square and click to play an X or an O."
+        boardDivs.forEach(div => {
+            if(div.classList.contains("X")){
+                div.classList.remove("X")
+            }else if(div.classList.contains("O")){
+                div.classList.remove("O")
+            }
+            div.innerHTML = ""
         })
     })
 }
