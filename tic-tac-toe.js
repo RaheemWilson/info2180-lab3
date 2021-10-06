f913fdfc8d6c70acc3d660f95dce218d0f49b647
RaheemWilson <wilsonzackery16@gmail.com>
2021-10-05 21:31:01 -0500

    Added winner status functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,9 @@
+let grid = new Array(9).fill("");
 
 window.onload = function () {
     let board = document.getElementById("board");
     let boardDivs = [...board.children];
-    let grid = new Array(9).fill("");
+    
 
     var nextPlay = ["X", "O"][Math.round(Math.random())];
     
@@ -13,8 +14,8 @@ window.onload = function () {
         div.addEventListener('click', () => {
             div.classList.add(nextPlay)
             div.innerHTML = nextPlay;
-            
             grid[parseInt(div.id)] = nextPlay
+            // checkGrid()
             nextPlay = nextPlay === "X" ? "O" : "X";      
         })
 
@@ -26,6 +27,6 @@ window.onload = function () {
             div.classList.remove("hover")
         })
     })
-    
-    
 }
+
+
