724da090be8e48411eb45370e9aa72bc6474f336
RaheemWilson <wilsonzackery16@gmail.com>
2021-10-05 20:52:19 -0500

    Added event listeners

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,23 @@
 
 window.onload = function () {
     let board = document.getElementById("board");
-    let boardDivs = [...board.children]
+    let boardDivs = [...board.children];
+    let grid = new Array(9).fill("");
 
+    var nextPlay = ["X", "O"][Math.round(Math.random())];
+    
+    boardDivs.forEach((div, index) => {  
+        div.className = "square"
+        div.id = `${index}`
 
-    for(let i = 0; i < boardDivs.length; i++){
-        boardDivs[i].className = "square"
-    }
+        div.addEventListener('click', () => {
+            div.classList.add(nextPlay)
+            div.innerHTML = nextPlay;
+            
+            grid[parseInt(div.id)] = nextPlay
+            nextPlay = nextPlay === "X" ? "O" : "X";      
+        })
+    })
+    
+    
 }
