1a334d15eec010bb2579f6e29296e039bb93cc6f
RaheemWilson <wilsonzackery16@gmail.com>
2021-10-07 17:54:25 -0500

    Fixed bug in code

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,7 @@
 
 
 window.onload = function () {
-    let grid = new Array(9).fill("");
+    let grid = new Array(9).fill(" ");
     let status = document.getElementById("status")
     let board = document.getElementById("board");
     let boardDivs = [...board.children];
@@ -9,42 +9,48 @@ window.onload = function () {
 
     var nextPlay = ["X", "O"][Math.round(Math.random())];
     
+    //Adds the appropriate class names as well as listeners to the squares
     boardDivs.forEach((div, index) => {  
         div.className = "square"
         div.id = `${index}`
 
+        //Add letter to square when user clicks it
         div.addEventListener('click', () => {
-            div.classList.add(nextPlay)
-            div.innerHTML = nextPlay;
-            grid[parseInt(div.id)] = nextPlay
-            checkGrid()
-            nextPlay = nextPlay === "X" ? "O" : "X";      
+            if(div.innerHTML === ""){
+                div.classList.add(nextPlay)
+                div.innerHTML = nextPlay;
+                grid[parseInt(div.id)] = nextPlay
+                checkGrid()
+                nextPlay = nextPlay === "X" ? "O" : "X";      
+            }
         })
 
+        //Hover effect enabled
         div.addEventListener('mouseover', () => {
             div.classList.add("hover")
         })
-
+        //Hover effect disabled
         div.addEventListener('mouseout', () => {
             div.classList.remove("hover")
         })
     })
     
+    //Function to check who wins
     const checkGrid = () => {
-    
         let gridstr = grid.join("")
-        console.log(gridstr)
-        if(gridstr.includes("XXX")){
+        if(gridstr.slice(0, 3).includes("XXX") || gridstr.slice(3, 6).includes("XXX") || gridstr.slice(6, 9).includes("XXX")){
             status.classList.add("you-won")
             status.innerHTML = "Congratulations! X is the Winner!"
-        } else if(gridstr.includes("OOO")){
+        } else if(gridstr.slice(0, 3).includes("OOO") || gridstr.slice(3, 6).includes("OOO") || gridstr.slice(6, 9).includes("OOO")){
             status.classList.add("you-won")
             status.innerHTML = "Congratulations! O is the Winner!"
         }
     }
 
+    //Reset button listener
     let btn = document.getElementsByClassName("btn")[0]
     btn.addEventListener('click', () => {
+        grid = new Array(9).fill(" ");
         status.classList.remove("you-won")
         status.innerHTML = "Move your mouse over a square and click to play an X or an O."
         boardDivs.forEach(div => {
